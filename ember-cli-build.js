af66ac83d9514933c09990d6b2bf05b4fdb960f2
abdullacontractor <abdulla.contractor@gmail.com>
2018-10-06 13:37:39 +0800

    service workers

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-service-worker': {
+      registrationStrategy: 'inline'
+    },
+    'esw-cache-fallback': {
+      patterns: [
+        'https://wt-338cb734ff2c818318cae0dffd078606-0.sandbox.auth0-extend.com/ember-quickstart/(.+)'
+      ],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
