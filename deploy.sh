edc7f0ed8f70737b46bb4bf046ebe06b5bb2efee
abdullacontractor <abdulla.contractor@gmail.com>
2018-10-17 23:17:26 +0800

    redirect http traffic

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "===== Beginning deploy"
 rm -rf dist
 ember build --environment=production
 mv dist/index.html dist/200.html
-surge dist abdullas-ember-playground.surge.sh
+surge dist https://abdullas-ember-playground.surge.sh
 
 echo "===== Finished deploy"
